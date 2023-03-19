85f9472e9b4885c0dc919b7a3a18c31ff160c573
mahdibagheri <bagheri.almas@gmail.com>
2023-03-20 00:10:44 +0330

    - make unique name for uploaded image - define __str__ for get food name

--- foods/models.py
+++ foods/models.py
@@ -1,4 +1,12 @@
+import uuid
+
 from django.db import models
+
+
+def image_upload_to(instance, filename):
+    ext = filename.split('.')[-1]
+    return 'food/{}-{}.{}'.format(instance.name, uuid.uuid4().hex, ext)
+
 
 class Food(models.Model):
     name = models.CharField(max_length=100)
@@ -7,4 +15,7 @@ class Food(models.Model):
     price = models.PositiveIntegerField()
     time_prepare = models.IntegerField()
     pub_date = models.DateTimeField(auto_now_add=True)
-    photo = models.ImageField(upload_to='foods/')
+    photo = models.ImageField(upload_to=image_upload_to)
+
+    def __str__(self):
+        return self.name
