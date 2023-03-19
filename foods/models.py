e332a2b9ceb9b444149833dd7eb625b210be7c76
mahdibagheri <bagheri.almas@gmail.com>
2023-03-19 23:41:14 +0330

    create Food model

--- foods/models.py
+++ foods/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Food(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+    rate = models.SmallIntegerField(default=0)
+    price = models.PositiveIntegerField()
+    time_prepare = models.IntegerField()
+    pub_date = models.DateTimeField(auto_now=True,auto_now_add=True)
+    photo = models.ImageField(upload_to='foods/')
