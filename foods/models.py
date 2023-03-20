cfa02e3fb9bbdaea9da0e5d4222de37dd53e0dc0
mahdibagheri <bagheri.almas@gmail.com>
2023-03-20 16:02:45 +0330

    - make rate range between 0 and 5 - change price and time_prepare to positiveSmallInteger

--- foods/models.py
+++ foods/models.py
@@ -9,11 +9,26 @@ def image_upload_to(instance, filename):
 
 
 class Food(models.Model):
+    FOOD_CATEGORY = [
+        ('breakfast', 'Breakfast'),
+        ('drinks', 'Drinks'),
+        ('main', 'Main'),
+    ]
+
+    RATE_CHOICES = [
+        (0, '0'),
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+    ]
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=300)
-    rate = models.SmallIntegerField(default=0)
-    price = models.PositiveIntegerField()
-    time_prepare = models.IntegerField()
+    category = models.CharField(max_length=15, choices=FOOD_CATEGORY, default='main')
+    rate = models.PositiveSmallIntegerField(choices=RATE_CHOICES,default=0,)
+    price = models.PositiveSmallIntegerField()
+    time_prepare = models.PositiveSmallIntegerField()
     pub_date = models.DateTimeField(auto_now_add=True)
     photo = models.ImageField(upload_to=image_upload_to)
 
