16fadffda5977330bdf5593d4c3bf66d862aa833
mahdibagheri <bagheri.almas@gmail.com>
2023-03-20 15:27:23 +0330

    create food_list view function

--- foods/views.py
+++ foods/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from foods.models import Food
+
+
+def food_list(request):
+    foods = Food.objects.all()
+    return render(request, 'base.html', {'foods': foods})
