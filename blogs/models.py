18d0cda749ee9d046a1e447c5ebede7e1d151169
mahdibagheri <bagheri.almas@gmail.com>
2023-03-21 22:37:05 +0330

    create models : - Tag - Category - Blog

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30)
+    is_enabled = models.BooleanField(default=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    is_enabled = models.BooleanField(default=True)
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField('Tag')
+    categories = models.ManyToManyField('Category')
+    created = models.DateTimeField(auto_now_add=True)
