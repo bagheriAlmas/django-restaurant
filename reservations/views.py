194b5049d98e911bc831b0d0aba776e68ce0adab
mahdibagheri <bagheri.almas@gmail.com>
2023-03-21 20:49:27 +0330

    add reserve_View in views.py

--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from reservations.forms import ReservationForm
+
+
+def reserve_view(request):
+    form = ReservationForm()
+    if request.method == 'POST':
+        form = ReservationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = ReservationForm()
+    return render(request, 'foods/reservation.html', {'form':form})
