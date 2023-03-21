78fb000efb65431521c7f15fa59eef202fc74fb9
mahdibagheri <bagheri.almas@gmail.com>
2023-03-21 20:23:50 +0330

    create food detail view

--- foods/views.py
+++ foods/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from foods.models import Food
 
@@ -6,3 +6,8 @@ from foods.models import Food
 def food_list(request):
     foods = Food.objects.all()
     return render(request, 'foods/food_list.html', {'foods': foods})
+
+
+def food_detail(request, pk):
+    food = get_object_or_404(Food, pk=pk)
+    return render(request, 'foods/food_detail.html', {'food': food})
