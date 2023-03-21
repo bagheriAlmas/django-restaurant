717f692c268dad5488db8e3aba636cf1eb598121
mahdibagheri <bagheri.almas@gmail.com>
2023-03-21 20:24:03 +0330

    add food detail url

--- foods/urls.py
+++ foods/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import food_list
+from .views import food_list, food_detail
+
 urlpatterns = [
-    path('foods/', food_list, name='foods')
+    path('foods/', food_list, name='foods'),
+    path('foods/<int:pk>', food_detail, name='food_detail'),
 ]
