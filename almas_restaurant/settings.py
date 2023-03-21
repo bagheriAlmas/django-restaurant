f4ee51d2f1d200491e59c5a93882ab02928bd2c8
mahdibagheri <bagheri.almas@gmail.com>
2023-03-21 22:37:46 +0330

    add blogs app to INSTALLED_APP

--- almas_restaurant/settings.py
+++ almas_restaurant/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'foods',
     'reservations',
+    'blogs',
 ]
 
 MIDDLEWARE = [
@@ -56,8 +57,7 @@ ROOT_URLCONF = 'almas_restaurant.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
