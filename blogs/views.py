6b18a92edbc63b66800cdf5009ea41b4fd6025a9
mahdibagheri <bagheri.almas@gmail.com>
2023-03-22 23:24:19 +0330

    create blog_detail view

--- blogs/views.py
+++ blogs/views.py
@@ -6,3 +6,8 @@ from blogs.models import Blog
 def blog_list_view(request):
     blogs = Blog.objects.all()
     return render(request, 'blogs/blog_list.html', {'blogs': blogs})
+
+
+def blog_detail_view(request,pk):
+    blog = Blog.objects.get(pk=pk)
+    return render(request, 'blogs/blog_detail.html', {'blog': blog})
