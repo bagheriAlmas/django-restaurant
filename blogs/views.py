4aad43ce7e7f54607f92d1e83c370c9554736cad
mahdibagheri <bagheri.almas@gmail.com>
2023-03-24 19:47:13 +0330

    - add comments to blogs views.py - def blog_tag_view - def blog_category_view

--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from blogs.models import Blog
+from comments.models import Comment
 
 
 def blog_list_view(request):
@@ -11,5 +12,16 @@ def blog_list_view(request):
 def blog_detail_view(request, pk):
     blog = Blog.objects.get(pk=pk)
     last_blogs = Blog.objects.order_by('-created')[0:4]
+    comments = Comment.objects.filter(blog=blog).all()
 
-    return render(request, 'blogs/blog_detail.html', {'blog': blog, 'last_blogs': last_blogs})
+    return render(request, 'blogs/blog_detail.html', {'blog': blog, 'last_blogs': last_blogs, 'comments': comments})
+
+
+def blog_tag_view(request, pk):
+    blogs = Blog.objects.all().filter(tags__pk=pk)
+    return render(request, 'blogs/blog_list.html', {'blogs': blogs})
+
+
+def blog_category_view(request, pk):
+    blogs = Blog.objects.filter(categories__pk=pk).all()
+    return render(request, 'blogs/blog_list.html', {'blogs': blogs})
