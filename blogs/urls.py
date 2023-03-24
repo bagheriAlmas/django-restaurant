ae045e710b9d6081bb9c5d35e5e215bf6c0bcf37
mahdibagheri <bagheri.almas@gmail.com>
2023-03-24 19:47:30 +0330

    - add url blog_tag_view - add url blog_category_view

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import blog_list_view, blog_detail_view
+from .views import blog_list_view, blog_detail_view, blog_tag_view, blog_category_view
 
 urlpatterns = [
     path('blogs/', blog_list_view, name='blog_list_view'),
     path('blogs/<int:pk>', blog_detail_view, name='blog_detail_view'),
+    path('tag/<int:pk>', blog_tag_view, name='blog_tag_view'),
+    path('category/<int:pk>', blog_category_view, name='blog_category_view'),
 ]
