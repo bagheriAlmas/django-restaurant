6c40507acf6cbf95afe2ee8ef4d46fbb805a655e
mahdibagheri <bagheri.almas@gmail.com>
2023-03-24 23:56:52 +0330

    - pagination for blog_list, blog_tag, blgo_category - define search_view

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 from blogs.models import Blog
@@ -6,7 +7,10 @@ from comments.models import Comment
 
 def blog_list_view(request):
     blogs = Blog.objects.all()
-    return render(request, 'blogs/blog_list.html', {'blogs': blogs})
+    paginator = Paginator(blogs, 6)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'blogs/blog_list.html', {'blogs': page_obj})
 
 
 def blog_detail_view(request, pk):
@@ -19,9 +23,28 @@ def blog_detail_view(request, pk):
 
 def blog_tag_view(request, pk):
     blogs = Blog.objects.all().filter(tags__pk=pk)
-    return render(request, 'blogs/blog_list.html', {'blogs': blogs})
+    paginator = Paginator(blogs, 6)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'blogs/blog_list.html', {'blogs': page_obj})
 
 
 def blog_category_view(request, pk):
     blogs = Blog.objects.filter(categories__pk=pk).all()
-    return render(request, 'blogs/blog_list.html', {'blogs': blogs})
+    paginator = Paginator(blogs, 6)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'blogs/blog_list.html', {'blogs': page_obj})
+
+
+def search_view(request):
+    try:
+        if request.method == 'GET':
+            str_search = request.GET.get('search')
+            blogs = Blog.objects.filter(title__icontains=str_search)
+            paginator = Paginator(blogs, 6)
+            page_number = request.GET.get('page')
+            page_obj = paginator.get_page(page_number)
+            return render(request, 'blogs/blog_list.html', {'blogs': page_obj})
+    except:
+        return render(request, 'foods/food_list.html', {})
