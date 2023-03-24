0fc99efc61caf4bf269a73fa47debccc6b15ab12
mahdibagheri <bagheri.almas@gmail.com>
2023-03-24 19:48:19 +0330

    create comments model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from blogs.models import Blog
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='Comments')
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name='Blogs')
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.owner) + " |==> " + str(self.content)
