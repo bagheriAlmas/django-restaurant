1224a84427b7007b069643852ec940bb24eb855e
mahdibagheri <bagheri.almas@gmail.com>
2023-03-24 19:52:27 +0330

    customize Blog in admin panel

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Blog, Tag, Category
 
-admin.site.register(Blog, )
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created')
+    filter_horizontal = ['tags', 'categories']
+    list_filter = ['categories',]
+
+
+admin.site.register(Blog, BlogAdmin)
 admin.site.register(Category)
 admin.site.register(Tag)
