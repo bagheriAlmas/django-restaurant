ee3685c2c05c314e0d3431caeb934b0690d01c3d
mahdibagheri <bagheri.almas@gmail.com>
2023-03-27 20:06:53 +0330

    make Staff Class

--- staffs/models.py
+++ staffs/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Staff(models.Model):
+    name = models.CharField(max_length=50)
+    position = models.CharField(max_length=50)
+    avatar = models.ImageField(upload_to='staffs/avatars')
+    email = models.EmailField(max_length=200)
+    twitter = models.CharField(max_length=200)
+    facebook = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
