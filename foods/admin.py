a141a8425841cf64610787e02ddc0008fa0720dd
mahdibagheri <bagheri.almas@gmail.com>
2023-03-27 13:20:07 +0330

    register Gallery model in admin panel

--- foods/admin.py
+++ foods/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from foods.models import Food
+from foods.models import Food,Gallery
 
 
 class FoodAdmin(admin.ModelAdmin):
@@ -7,4 +7,10 @@ class FoodAdmin(admin.ModelAdmin):
     list_display = ('name', 'category')
 
 
+class GalleryAdmin(admin.ModelAdmin):
+    model = Gallery
+    list_display = ('title', 'is_enabled')
+
+
 admin.site.register(Food,FoodAdmin)
+admin.site.register(Gallery,GalleryAdmin)
