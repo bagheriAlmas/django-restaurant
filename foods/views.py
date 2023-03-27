5e5dfe15fefdaf82499e4558fbb3d188e9f0c595
mahdibagheri <bagheri.almas@gmail.com>
2023-03-27 15:07:58 +0330

    add menu_list view

--- foods/views.py
+++ foods/views.py
@@ -3,11 +3,16 @@ from django.shortcuts import render, get_object_or_404
 from foods.models import Food, Gallery
 
 
-def food_list(request):
+def special_food_list(request):
     foods = Food.objects.all()
     pictures = Gallery.objects.all().filter(type='gallery')
     banners = Gallery.objects.all().filter(type='banner')
     return render(request, 'foods/food_list.html', {'foods': foods, 'pictures': pictures, 'banners': banners})
+
+
+def menu_list(request):
+    foods = Food.objects.all()
+    return render(request, 'foods/food_menu.html', {'foods': foods,})
 
 
 def food_detail(request, pk):
