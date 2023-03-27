ce23f7a781a595356f31445cc2217eea101eb1b6
mahdibagheri <bagheri.almas@gmail.com>
2023-03-27 15:54:43 +0330

    set foods view for defautl url of project

--- foods/urls.py
+++ foods/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import special_food_list, food_detail,menu_list
 
 urlpatterns = [
-    path('foods/', special_food_list, name='foods'),
+    path('', special_food_list, name='foods'),
     path('foods/<int:pk>', food_detail, name='food_detail'),
     path('foods/menu/', menu_list, name='menu'),
 ]
