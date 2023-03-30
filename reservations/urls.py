011b95b263ba6a4294359c0f84a6e8964dfa970c
mahdibagheri <bagheri.almas@gmail.com>
2023-03-30 19:28:53 +0330

    create contact_us_view url

--- reservations/urls.py
+++ reservations/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import reserve_view
+from .views import reserve_view,contact_us_view
 
 urlpatterns = [
-    path('reservations/', reserve_view, name='reserve_view')
+    path('reservations/', reserve_view, name='reserve_view'),
+    path('contact-us/', reserve_view, name='contact_us'),
 ]
