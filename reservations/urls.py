46ff94e9c75349eebfaf575433fe31263785f9e3
mahdibagheri <bagheri.almas@gmail.com>
2023-03-30 20:31:42 +0330

    fix contact-us url

--- reservations/urls.py
+++ reservations/urls.py
@@ -3,5 +3,5 @@ from .views import reserve_view,contact_us_view
 
 urlpatterns = [
     path('reservations/', reserve_view, name='reserve_view'),
-    path('contact-us/', reserve_view, name='contact_us'),
+    path('contact-us/', contact_us_view, name='contact_us'),
 ]
