1dc923a4e03d7a44e93ef1221e8088fccd6b64c4
mahdibagheri <bagheri.almas@gmail.com>
2023-03-30 19:29:09 +0330

    create ContactUsForm form

--- reservations/forms.py
+++ reservations/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Reservation
+from .models import Reservation,ContactUs
 
 
 class ReservationForm(forms.ModelForm):
     class Meta:
         model = Reservation
+        fields = '__all__'
+
+
+class ContactUsForm(forms.ModelForm):
+    class Meta:
+        model = ContactUs
         fields = '__all__'
