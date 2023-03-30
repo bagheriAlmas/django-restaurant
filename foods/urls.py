ff67777c2ba8dfd35be490a4533b796f60850f04
mahdibagheri <bagheri.almas@gmail.com>
2023-03-30 18:31:53 +0330

    make gallery url in foods urls

--- foods/urls.py
+++ foods/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import special_food_list, food_detail,menu_list
+from .views import special_food_list_view, food_detail_view, menu_list_view, gallery_list_view
 
 urlpatterns = [
-    path('', special_food_list, name='foods'),
-    path('foods/<int:pk>', food_detail, name='food_detail'),
-    path('foods/menu/', menu_list, name='menu'),
+    path('', special_food_list_view, name='foods'),
+    path('foods/<int:pk>', food_detail_view, name='food_detail'),
+    path('foods/menu/', menu_list_view, name='menu'),
+    path('foods/gallery/', gallery_list_view, name='gallery'),
 ]
