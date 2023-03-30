2fc145c6e1e06a9b321d465cd5d9390b47b784a1
mahdibagheri <bagheri.almas@gmail.com>
2023-03-30 19:28:30 +0330

    create ContactUs model

--- reservations/models.py
+++ reservations/models.py
@@ -11,3 +11,14 @@ class Reservation(models.Model):
 
     def __str__(self):
         return self.name + " - " + self.phone
+
+
+class ContactUs(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    phone = models.CharField(max_length=20)
+    message = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name + " - " + self.phone
