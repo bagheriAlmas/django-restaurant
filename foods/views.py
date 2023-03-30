4355c4e7fc73bd41f62cf8ab827a4921cef08331
mahdibagheri <bagheri.almas@gmail.com>
2023-03-30 18:32:40 +0330

    - create gallery view in foods views - change functions name to standard views names

--- foods/views.py
+++ foods/views.py
@@ -3,18 +3,23 @@ from django.shortcuts import render, get_object_or_404
 from foods.models import Food, Gallery
 
 
-def special_food_list(request):
+def special_food_list_view(request):
     foods = Food.objects.all()
     pictures = Gallery.objects.all().filter(type='gallery')
     banners = Gallery.objects.all().filter(type='banner')
     return render(request, 'foods/food_list.html', {'foods': foods, 'pictures': pictures, 'banners': banners})
 
 
-def menu_list(request):
+def menu_list_view(request):
     foods = Food.objects.all()
     return render(request, 'foods/food_menu.html', {'foods': foods,})
 
 
-def food_detail(request, pk):
+def food_detail_view(request, pk):
     food = get_object_or_404(Food, pk=pk)
     return render(request, 'foods/food_detail.html', {'food': food})
+
+
+def gallery_list_view(request):
+    gallery = Gallery.objects.all()
+    return render(request, 'pages/gallery.html', {'gallery': gallery})
